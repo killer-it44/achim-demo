ee348468c182b0a2e201c7459bfc02301e612b89
xiaoye999 <keller_jens@gmx.de>
2020-06-21 13:56:15 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
  
 app.get('/', function (req, res) {
-  res.send('Hello from Achim')
+  res.send('Hello from Achim!')
 })
  
 app.listen(process.env.PORT || 3000)
